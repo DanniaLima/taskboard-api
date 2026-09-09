0927c10c9d75a0291bcd83e682eddf65bdc78551
DanniaLima <dannia.lima@gmail.com>
2026-09-09 17:08:31 +0200

    Initial Spring Boot setup with health check endpoint

---
+++ src/test/java/com/dannialima/taskboard/TaskboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dannialima.taskboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
